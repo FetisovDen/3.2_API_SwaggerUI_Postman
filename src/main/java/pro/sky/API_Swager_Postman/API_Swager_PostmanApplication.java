e0de5b95fed9625ebe21ff06ce4c4f4fdd0fff61
555 <fetisovden@yandex.ru>
2022-11-02 14:24:41 +0300

    1st commit

---
+++ src/main/java/pro/sky/API_Swager_Postman/API_Swager_PostmanApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.API_Swager_Postman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class API_Swager_PostmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(API_Swager_PostmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/API_Swager_Postman/APISwagerPostmanApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.API_Swager_Postman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APISwagerPostmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
